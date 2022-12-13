65b909a94d6b3873495763a9ccd9bc83340785fd
Nacho1886 <francert@hotmail.com>
2022-12-13 19:17:37 +0100

    Trying to create event emitter

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { DisplayService } from '../../services/display.service';
+import { emitDistinctChangesOnlyDefaultValue } from '@angular/compiler';
+import { Component, ViewChild, ElementRef, EventEmitter } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { SafeSubscriber } from 'rxjs/internal/Subscriber';
 
 @Component({
   selector: 'app-navbar',
@@ -7,23 +9,42 @@ import { DisplayService } from '../../services/display.service';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
-  desktopVersion!: boolean
 
-  
-  constructor(private displayService: DisplayService) {
-    this.displayService.observer.observe()
+  desktopVersion!: EventEmitter = new EventEmitter(true)
+  @ViewChild('boxContainer') boxContainer!: ElementRef
+
+  constructor() { }
+
+
+  ngAfterViewInit(): void {
+    this.desktopVersion = new ResizeObservable(this.boxContainer.nativeElement).subscribe(resized => {
+      const withSize = resized[0].contentRect.width
+      console.log("🚀 ~ file: navbar.component.ts:21 ~ NavbarComponent ~ this.desktopVersion=newResizeObservable ~ withSize", withSize)
+      if (withSize >= 768) new EventEmitter(true)
+      console.log("🚀 ~ file: navbar.component.ts:24 ~ NavbarComponent ~ this.desktopVersion=newResizeObservable ~ new EventEmitter(true)", new EventEmitter())
+      if (withSize >= 768) return true
+      if (withSize <= 768) return false
+      return
+    })
     
   }
 
-  /* changeVersion() {
-    return this.version$.asObservable()
-  } */
-  
-  ngOnInit(): void {
-    this.displayService.changeVersion()
-    console.log(matchMedia("(max-width: 768px)"));
-    // console.log(onresize());
-    
-  }
+}
 
+
+export class ResizeObservable extends Observable<ResizeObserverEntry[]> {
+  constructor(elem: HTMLElement) {
+    super(subscriber => {
+      const ro = new ResizeObserver(entries => {
+        subscriber.next(entries);
+      });
+
+      ro.observe(elem);
+
+      return function unsubscribe() {
+        ro.unobserve(elem);
+        ro.disconnect();
+      }
+    });
+  }
 }
