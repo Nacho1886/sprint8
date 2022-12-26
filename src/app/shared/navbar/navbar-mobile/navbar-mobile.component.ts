0638c7ee27a9bc1fbc764f66f155f4d47e552d36
Nacho1886 <francert@hotmail.com>
2022-12-26 16:45:28 +0100

    Menu mobile fixed

--- src/app/shared/navbar/navbar-mobile/navbar-mobile.component.ts
+++ src/app/shared/navbar/navbar-mobile/navbar-mobile.component.ts
@@ -6,11 +6,23 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
   styleUrls: ['./navbar-mobile.component.scss']
 })
 export class NavbarMobileComponent {
+  @ViewChild('burger') burger!: ElementRef
   @ViewChild('navMobile') navMobile!: ElementRef
+
   constructor() { }
   
-  hamburgerMenu(e: any) {
-    e.currentTarget.classList.toggle("active")
+  displayMenu() {
+    this.burger.nativeElement.classList.toggle("active")
     this.navMobile.nativeElement.classList.toggle("show")
+  }
+
+  ngOnInit(): void {
+    document.addEventListener('click', (event) => {
+      const condition1 = this.burger.nativeElement.classList.contains("active")
+      const condition2 = !this.burger.nativeElement.contains(event.target)
+      const condition3 = !this.navMobile.nativeElement.contains(event.target)
+
+      if (condition1 && condition2 && condition3) this.displayMenu()
+    })
   }
 }
