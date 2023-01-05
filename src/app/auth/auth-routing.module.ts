aa3d95d779a0363e47bfd0e7460727ae01bd63b2
Nacho1886 <francert@hotmail.com>
2023-01-05 18:17:39 +0100

    New components in login and register added

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -2,16 +2,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { RegisterGuard } from './guards/register.guard';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'login', component: LoginComponent ,
+    canActivateChild: [RegisterGuard],
     children: [
-      { path: 'login', component: LoginComponent },
-      { path: 'register', component: RegisterComponent },
-      { path: '**', redirectTo: 'login' }
+      {
+        path: 'register',
+        component: RegisterComponent
+      }
     ]
-  }
+  },
+  { path: '**', redirectTo: 'login' }
 ];
 
 @NgModule({
--- src/app/auth/guards/register.guard.ts
+++ src/app/auth/guards/register.guard.ts
@@ -1,15 +1,29 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivateChild, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivateChild, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegisterGuard implements CanActivateChild {
+
+  emailExist!: string
+
+  constructor(
+    private router: Router,
+    private authService: AuthService
+    ) {
+      this.authService.email.subscribe(email => this.emailExist = email)
+      console.log("🚀 ~ file: register.guard.ts:18 ~ RegisterGuard ~ this.emailExist", this.emailExist)
+  }
+
   canActivateChild(
     childRoute: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+      if (this.emailExist) return true
+      this.router.navigate(['/auth/login'])
+      return false
   }
   
 }
