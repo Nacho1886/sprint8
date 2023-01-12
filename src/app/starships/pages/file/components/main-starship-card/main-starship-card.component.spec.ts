00991a24e6436d29cc82cbaa1f9e565d079faa55
Nacho1886 <francert@hotmail.com>
2023-01-12 12:55:54 +0100

    Testing again

---
+++ src/app/starships/pages/file/components/main-starship-card/main-starship-card.component.spec.ts
@@ -0,0 +1,20 @@
+import { MainStarshipCardComponent } from "./main-starship-card.component"
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+
+
+describe('Main card Starship', () => {
+    let component = MainStarshipCardComponent
+    let fixture = ComponentFixture<MainStarshipCardComponent>
+
+    beforeEach(() => {
+
+        TestBed.configureTestingModule({
+            declarations: [ MainStarshipCardComponent ]
+          });
+
+        fixture = TestBed.createComponent(MainStarshipCardComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    })
+})
